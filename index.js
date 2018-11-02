764d76f4fb9a7507ee9ba0275358103741266553
Trevor <trevorhreed@gmail.com>
2018-11-02 12:31:55 -0600

    a few more changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = () => {
 }
 
 function EventBus () {
-  this.handlers = {}
+  this.fns = {}
   this.pending = []
 }
 
@@ -11,21 +11,36 @@ EventBus.prototype.settle = function () {
   return Promise.all(this.pending.slice())
 }
 
-EventBus.prototype.on = function (key, handler) {
-  if (!this.handlers[key]) this.handlers[key] = []
-  this.handlers[key].push(handler)
-}
-
-EventBus.prototype.off = function (key, handler) {
-  if (this.handlers[key]) {
-    this.handlers.splice(this.handlers.indexOf(handler), 1)
+EventBus.prototype.on = function (key, fn) {
+  if (typeof fn === 'function') {
+    (this.fn[key] = this.fn[key] || []).push(fn)
   }
+  return this  
 }
 
-EventBus.prototype.dispatch = function (key, ...args) {
+EventBus.prototype.one = function (key, fn) {
+  function once() {
+    this.off(key, once)
+    fn.apply(null, arguments)
+  }
+  return this.on(key, once)
+}
+
+EventBus.prototype.off = function (key, fn) {
+  if (!fn) {
+    delete this.fns[key]
+  } else if (this.fns[key]) {
+    for(let i=0, f; f = arr && arr[i]; i++) {
+      if (f === fn) arr.splice(i--, 1)
+    }
+  }
+  return this
+}
+
+EventBus.prototype.fire = function (key, ...args) {
   const promise = Promise.all(
-    this.handlers[key].map(handler => {
-      return Promise.resolve().then(x => handler(...args))
+    this.fns[key].map(fn => {
+      return Promise.resolve().then(x => fn(...args))
     })
   )
   this.pending.push(promise)
